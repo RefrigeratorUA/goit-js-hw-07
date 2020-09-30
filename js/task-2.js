4150368bee15ce4da75beec99d5c94d2d1b021e8
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2020-09-30 23:00:26 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,14 @@
-const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
+// после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
 
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
 console.log(ingredients);
+
+const ingredientsList = ingredients.map(ingredient => {
+  const listItemRef = document.createElement('li');
+  listItemRef.textContent = ingredient;
+  return listItemRef;
+});
+
+document.querySelector('#ingredients').append(...ingredientsList);
