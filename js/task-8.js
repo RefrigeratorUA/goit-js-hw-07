b754c6da9c29a77ddbcf1465d7a0c93a590fbb84
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2020-10-01 23:02:33 +0300

    task-8

--- js/task-8.js
+++ js/task-8.js
@@ -12,4 +12,41 @@
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
 
-console.log('task-8');
+const inputRef = document.querySelector('input');
+const divBoxesRef = document.querySelector('#boxes');
+let inputValue = 0;
+
+const buttonsRef = document.querySelectorAll('#controls button');
+let btnRender;
+let btnDestroy;
+buttonsRef.forEach(el => (el.dataset.action === 'render' ? (btnRender = el) : (btnDestroy = el)));
+
+inputRef.addEventListener('blur', () => (inputValue = Number(inputRef.value)));
+
+btnRender.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = inputValue;
+  const divArr = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    divArr[i] = document.createElement('div');
+    divArr[i].style.width = `${i * 10 + 30}px`;
+    divArr[i].style.height = `${i * 10 + 30}px`;
+    divArr[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    console.log(divArr[i]);
+  }
+
+  divBoxesRef.append(...divArr);
+}
+
+function destroyBoxes() {
+  divBoxesRef.innerHTML = '';
+  inputRef.value = '';
+  console.clear();
+}
+
+function randomColor() {
+  return Math.floor(Math.random() * (256 - 0)) + 0;
+}
